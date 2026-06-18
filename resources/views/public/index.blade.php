18bb1b3aad6095c1204c6f10606c2b531d8341b8
John Chidozie Nebo <johnchidozie320@gmail.com>
2026-06-18 09:56:57 +0100

    Removed title

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -292,7 +292,7 @@
 					<div class="header-bar">
 						<a href="index.html" class="branding">
 							<img src="{{asset('assets/image/images/rotationalLogo.gif')}}" alt="" class="logo" width="70" height="70">
-							<h1 class="site-title">People Of Exploits Ministry International</h1>
+							{{--<h1 class="site-title">People Of Exploits Ministry International</h1>--}}
 						</a>
 
 						<!-- Default snippet for navigation -->
